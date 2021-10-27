da709ea1cad8a205cd5df6643bb060d18e47a7de
Srikanth <srikanthh994@gmail.com>
2021-10-27 12:05:59 +0530

    Added:Validate Email

--- UserRegistration.java
+++ UserRegistration.java
@@ -5,9 +5,6 @@ import java.util.regex.Pattern;
 public class UserRegistration {
 	private Pattern pattern; // Declaring The Pattern variable as private
 
-	/*
-	 * Declaring Validate First Name Method
-	 */
 	public boolean validateFirstName(String firstName) {
 		// Here Declaring The Name Pattern
 		String Name_Pattern = "^[A-Z][a-z]{2,}$"; // The First Letter With Cap And Min 3 Letters
@@ -18,6 +15,13 @@ public class UserRegistration {
 		// Here Declaring The Name Pattern
 		String Name_pattern = "^[A-Z][a-z]{2,}$";// The First Letter With Cap And Min 3 Letters
 		return pattern.matches(Name_pattern, lastName);// Matching The Name Pattern And FirstName And Returning It.
+	}
+
+	// Declaring Valid Email Method
+	public boolean validateEmail(String Email) {
+		// Here Declaring The Email Pattern
+		String Email_pattern = "[a-z][a-zA-Z0-9]+([._+#-][a-zA-Z0-9]+)*@[a-zA-Z]{5}+.[a-zA-Z]{2,3}(.[a-zA-Z]{2})?$";
+		return pattern.matches(Email_pattern, Email);// Matching The Email Pattern And Email And Returning It.
 	}
 
 }
